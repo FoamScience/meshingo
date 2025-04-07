241580907f03721d5fde62d00f2ec4e169e5ddc6
Elwardi <elwardi.fadeli@tu-darmstadt.de>
2025-04-07 20:08:20 +0200

    fix(cli): add training_dataset argument to validate CLI command

--- app/src/validate_command.sh
+++ app/src/validate_command.sh
@@ -3,6 +3,9 @@ testing_stls="${git_root_folder}/testing_dataset"
 if [ "$(readlink -f "${args[target_stl]}")" != "$(readlink -f "$testing_stls/$(basename ${args[target_stl]})")" ]; then
     cp "${args[target_stl]}" "$testing_stls"
 fi
+if [ "$(readlink -f "${args[--training-set]}")" != "$(readlink -f "training_dataset/geometric_features.csv" ]; then
+    cp "${args[--training-set]}" training_dataset/geometric_features.csv
+fi
 echo -e "Computing geometrical features of all STL files found in ${testing_stls}"
 uv run compute-geometric-features  "${testing_stls}"
 echo -e "\e[32mValidation run \e[0mfor ${args[--model]} surrogate model..."
